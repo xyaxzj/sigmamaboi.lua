2ae8b89cfb38e70498545f7b56034637614d8830
xyaxzj <somethinc333@gmail.com>
2026-08-19 11:06:40 +0700

    Update autfamlabv2.lua

--- autfamlabv2.lua
+++ autfamlabv2.lua
@@ -37,7 +37,7 @@ local isMeteorShowerActive = false
 
 -- Ukuran hitbox optimal untuk Raycast (80x80x80 studs).
 -- JANGAN 2048 karena raycast yang mulai di dalam part 2048 akan tembus/miss!
-local OPTIMAL_HITBOX_SIZE = Vector3.new(150, 150, 150)
+local OPTIMAL_HITBOX_SIZE = Vector3.new(200, 200, 200)
 
 local activeMeteors = {}
 local processedRoots = {}
