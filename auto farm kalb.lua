015b35996b3890a9fa34ffb4365cca63c1236535
xyaxzj <somethinc333@gmail.com>
2026-09-20 22:34:00 +0700

    Update auto farm kalb.lua

--- auto farm kalb.lua
+++ auto farm kalb.lua
@@ -34,7 +34,7 @@ _G.brainrotWhitelist    = {         -- Daftar nama brainrot yang diizinkan (Case
 }
 _G.autoSellAll         = false       -- true: Auto Sell All setiap 5 detik via ref_B_SellAll
 _G.autoRemovePlayer    = true        -- true: Hapus player lain dari game.Players & workspace.Players (100% Bersih & No Lag), false: Biarkan
-_G.debugConsoleLog     = true        -- true: Cetak log status/fase/candy ke console (F9), false: Senyap
+_G.debugConsoleLog     = false        -- true: Cetak log status/fase/candy ke console (F9), false: Senyap
 _G.failsafeTimeout     = 25          -- Waktu maksimal (detik) sebelum auto-reset ke Safe Zone jika macet
 
 -- ⚡ ULTRA ANTI-LAG & POTATO MODE (PUSH MAX PERFORMANCE)
