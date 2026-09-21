1dede6e564580dcf310c35dc793f48c471b0c299
xyaxzj <somethinc333@gmail.com>
2026-09-22 01:11:09 +0700

    Update auto farm kalb.lua

--- auto farm kalb.lua
+++ auto farm kalb.lua
@@ -27,7 +27,7 @@ _G.onlyCandyEvent       = false       -- true: HANYA Auto Kick saat Candy Event 
 -- 🍬 PENGATURAN FITUR CANDY EVENT (DAPAT DIAKTIFKAN / DINONAKTIFKAN SECARA TERPISAH)
 _G.enableCandyEvent     = true        -- [1] Master Switch: Aktifkan penanganan Candy Event (cuaca & spawn permen)
 _G.expandCandyHitbox    = true        -- [2] Hitbox Switch: Memperbesar hitbox Candy/Cokelat/dll ke ukuran yang ditentukan
-_G.candyHitboxSize      = Vector3.new(50, 50, 50) -- Ukuran hitbox Candy yang dibesarkan
+_G.candyHitboxSize      = Vector3.new(100, 100, 100) -- Ukuran hitbox Candy yang dibesarkan
 _G.candyWaypointNav     = true        -- [3] Navigation Switch: Pandu rute jalan kaki melintasi waypoint permen ke Safe Zone
 _G.candyReachDist       = 1          -- Jarak (studs) horizontal untuk menganggap permen sudah terlewati/terambil
 _G.verifyDebrisPickup   = true        -- [4] Debris Checker: Pastikan barang di Debris hilang saat dibawa; jika belum hilang, kembali ke waypoint
