b9ddec109301fa21de3792ad9c49d70aef25942b
xyaxzj <somethinc333@gmail.com>
2026-09-23 11:01:25 +0700

    Update mmgicloot.lua

--- mmgicloot.lua
+++ mmgicloot.lua
@@ -1,5 +1,5 @@
 -- ==============================================================================
--- 🥔 KALB ULTRA LIGHTWEIGHT AUTO FARM V5.1 (SMOOTH SPRINT CANDY EDITION)
+-- 🥔 KALB ULTRA LIGHTWEIGHT AUTO FARM V5.2 (SMOOTH SPRINT CANDY EDITION)
 -- ==============================================================================
 -- Fitur & Alur:
 -- 1. ⚙️ Full Config Mode: Semua pengaturan diatur via variabel _G di baris atas (Tanpa UI)
